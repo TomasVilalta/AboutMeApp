3f394602be9c65a28945a8d097538e0b380b52ef
TomasVilalta <tomasfedericovilalta@gmail.com>
2022-04-19 10:18:46 -0300

    enabled data binding

--- app/src/main/java/com/example/aboutmeapp/MainActivity.kt
+++ app/src/main/java/com/example/aboutmeapp/MainActivity.kt
@@ -1,41 +1,31 @@
 package com.example.aboutmeapp
 
-import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutmeapp.databinding.ActivityMainBinding
 
 //You were supposed to build the UI of this app using the layout editor
 //I did it  writing the xml because idk
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.done_button)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-
-        button.setOnClickListener(){
-            displayName(button)
-
-        }
-
-
+        binding.doneButton.setOnClickListener(){
+            displayName(binding.doneButton) }
     }
 
-
     private fun displayName(view: View) {
-        val editText  = findViewById<EditText>(R.id.name_et)
-        val name = findViewById<TextView>(R.id.name_text)
-        name.text = editText.text
-        editText.visibility= View.GONE
+        binding.apply{
+        nameText.text = nameEt.text
+        nameEt.visibility= View.GONE
         view.visibility = View.GONE
-        name.visibility = View.VISIBLE
-
+        nameText.visibility = View.VISIBLE}
         //This closes the keyboard.
         //It should probably be a function
         val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
